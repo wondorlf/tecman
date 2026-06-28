1be6aff3b18b4ed6cd49de699a5f47ef8d2ae645
Jorge Luis Montiel Sterling <jorgeluismontiel@outlook.com>
2026-06-27 19:18:19 -0500

    Actualizacion

--- backend/.adminjs/bundle.js
+++ backend/.adminjs/bundle.js
@@ -1,1 +1,12 @@
-(function(n){"use strict";function t(o){return o&&o.__esModule?o:{default:o}}var e=t(n);AdminJS.UserComponents={},AdminJS.UserComponents.LogoUpload=e.default})(LogoUpload);
+(function (LogoUpload) {
+	'use strict';
+
+	function _interopDefault (e) { return e && e.__esModule ? e : { default: e }; }
+
+	var LogoUpload__default = /*#__PURE__*/_interopDefault(LogoUpload);
+
+	AdminJS.UserComponents = {};
+	AdminJS.UserComponents.LogoUpload = LogoUpload__default.default;
+
+})(LogoUpload);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlcyI6WyJlbnRyeS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJBZG1pbkpTLlVzZXJDb21wb25lbnRzID0ge31cbmltcG9ydCBMb2dvVXBsb2FkIGZyb20gJ2NvbXBvbmVudHMvTG9nb1VwbG9hZCdcbkFkbWluSlMuVXNlckNvbXBvbmVudHMuTG9nb1VwbG9hZCA9IExvZ29VcGxvYWQiXSwibmFtZXMiOlsiQWRtaW5KUyIsIlVzZXJDb21wb25lbnRzIiwiTG9nb1VwbG9hZCJdLCJtYXBwaW5ncyI6Ijs7Ozs7OztDQUFBQSxPQUFPLENBQUNDLGNBQWMsR0FBRyxFQUFFO0NBRTNCRCxPQUFPLENBQUNDLGNBQWMsQ0FBQ0MsVUFBVSxHQUFHQSwyQkFBVTs7Ozs7OyJ9
